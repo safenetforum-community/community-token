aa153f151194e0c87c89fdb81b8f1d2240c1f3cb
loziniak <maciej@robotix-lozinski.pl>
2025-05-15 01:29:24 +0200

    [WIP] wallet: create token, refactoring

--- act-wallet/src-tauri/src/lib.rs
+++ act-wallet/src-tauri/src/lib.rs
@@ -1,25 +1,26 @@
 use futures::lock::Mutex;
 use tauri::{AppHandle, State, Manager};
 use autonomi::{Client, SecretKey, Wallet};
+use ant_act::ActExt;
 
 
 struct AppState {
-	client: Option<Client>,
-	wallet: Option<Wallet>,
+	client: Client,
+	wallet: Wallet,
+	sk: SecretKey,
 }
 
 
 #[tauri::command]
-async fn connect(local: bool, evm_pk: Option<String>, state: State<'_, Mutex<AppState>>) -> Result<(), String> {
+async fn connect(local: bool, evm_pk: Option<String>, state: State<'_, Mutex<Option<AppState>>>) -> Result<(), String> {
 	let mut state = state.lock().await;
 
-	if state.client.is_none() {
+	if state.is_none() {
 		let client = if local {
 			Client::init_local().await
 		} else {
 			Client::init().await
 		}.map_err(|e| format!("{}", e))?;
-
 
 		let evm_pk = evm_pk.unwrap_or(SecretKey::random().to_hex()); // bls secret key can be used as eth privkey
 		let wallet = Wallet::new_from_private_key(client.evm_network().clone(), &evm_pk)
@@ -27,19 +28,54 @@ async fn connect(local: bool, evm_pk: Option<String>, state: State<'_, Mutex<App
 
 		println!("EVM Address: {}", wallet.address());
 
-		state.client = Some(client);
-		state.wallet = Some(wallet);
+		let sk = SecretKey::from_bytes(sn_bls_ckd::derive_master_sk(
+			hex::decode(evm_pk).map_err(|e| format!("{}", e))?[0..32].try_into().unwrap()
+		).expect("Wrong bytes").serialize().into()).expect("Wrong bytes");
+
+		state = AppState {
+			client,
+			wallet,
+			sk
+		}
 	}
 
 	Ok(())
 }
 
-
 #[tauri::command]
-async fn balance(state: State<'_, Mutex<AppState>>) -> Result<(String, String), String> {
+async fn create_token(
+	name: String,
+	symbol: String,
+	decimals: u8,
+	total_supply: String,
+	state: State<'_, Mutex<Option<AppState>>>
+) -> Result<String, String> {
 	let mut state = state.lock().await;
 
-	let wallet = state.wallet.clone().ok_or("Not connected.")?;
+	let client = state.ok_or("Not connected.")?.client.clone();
+	let sk = state.ok_or("Not connected.")?.sk.clone();
+
+//	let act_wallet = ant_act::Wallet::new();
+	let act_wallet = client.act_wallet(sk);
+	let owner = act_wallet.request(DerivationIndex(vec![1]));
+
+	let total_supply = U256::from_str_radix(&total_supply, 10).map_err(|e| format!("{}", e))?;
+	let genesis_spend = client.act_create(
+		name,
+		symbol,
+		decimals,
+		total_supply,
+		owner,
+	).await.map_err(|e| format!("{}", e))?;
+
+	Ok(hex::encode(genesis_spend.0))
+}
+
+#[tauri::command]
+async fn balance(state: State<'_, Mutex<Option<AppState>>>) -> Result<(String, String), String> {
+	let mut state = state.lock().await;
+
+	let wallet = state.ok_or("Not connected.")?.wallet.clone();
 
 	let ant = format!("{}", wallet.balance_of_tokens().await.map_err(|e| format!("{}", e))?);
 	let eth = format!("{}", wallet.balance_of_gas_tokens().await.map_err(|e| format!("{}", e))?);
--- act-wallet/src/main.ts
+++ act-wallet/src/main.ts
@@ -1,8 +1,8 @@
 import { invoke } from "@tauri-apps/api/core";
 
-let pkInputEl: HTMLInputElement | null;
-
 async function connect(local: boolean) {
+  const pkInputEl = document.querySelector("#pk-input");
+
   if (pkInputEl) {
     const pk = pkInputEl.value || null;
     console.log("Connecting Autonomi...");
@@ -12,18 +12,75 @@ async function connect(local: boolean) {
     });
     console.log("Connected.");
 
-    let balance = await invoke("balance");
-    let balanceEl = document.createElement("p");
-    balanceEl.innerHTML = "Balance: " + balance;
-
-    document.getElementById("connect").after(balanceEl);
+    await balance();
     document.getElementById("connect").hidden = true;
   }
 }
 
+async function createToken() {
+  const nameInputEl = document.querySelector("#create-token name input");
+  const name = nameInputEl.value;
+  
+  const symbolInputEl = document.querySelector("#create-token symbol input");
+  const symbol = symbolInputEl.value;
+  
+  const supplyInputEl = document.querySelector("#create-token supply input");
+  const supply = supplyInputEl.value;
+
+  const decimalsInputEl = document.querySelector("#create-token decimals input");
+  const decimals = decimalsInputEl.value;
+
+  try {
+    const tokenId = await invoke("create_token", {
+      name: name,
+      symbol: symbol,
+      decimals: decimals,
+      totalSupply: supply,
+    });
+
+    message("Token ID: " + tokenId, "create-token")
+
+  } catch (e) {
+    error("" + e, "create-token");
+  }
+}
+
+async function balance() {
+  let balanceEl = document.querySelector("#balance");
+  if !balanceEl {
+    balanceEl = document.createElement("p");
+    balanceEl.id = "balance";
+    document.getElementById("connect").after(balanceEl);
+  }
+
+  const balance = await invoke("balance");
+  balanceEl.innerHTML = "Balance: " + balance;
+}
+
+function message(text: string, afterId: string) {
+  let msgEl = document.querySelector("#{afterId} ~ .message");
+  if !msgEl {
+    msgEl = document.createElement("p");
+    msgEl.className = "message";
+    document.getElementById(afterId).after(msgEl);
+  }
+  msgEl.innerHTML = text;
+}
+
+function error(text: string, afterId: string) {
+  let errEl = document.querySelector("#{afterId} ~ .error");
+  if !errEl {
+    errEl = document.createElement("p");
+    errEl.className = "error";
+    document.getElementById(afterId).after(errEl);
+  }
+  errEl.innerHTML = text;
+}
+
 window.addEventListener("DOMContentLoaded", () => {
-  pkInputEl = document.querySelector("#pk-input");
 
   document.querySelector("#main-connect-button")?.addEventListener("click", (e) => connect(false));
   document.querySelector("#local-connect-button")?.addEventListener("click", (e) => connect(true));
+
+  document.querySelector("#create-token button")?.addEventListener("click", (e) => createToken());
 });
