518b6b8f92272e45957b895463ebdfa87010182f
loziniak <maciej@robotix-lozinski.pl>
2025-05-21 09:28:54 +0200

    [WIP] wallet: create token, refactoring

--- token/src/lib.rs
+++ token/src/lib.rs
@@ -1,7 +1,10 @@
+use std::fmt;
 use ruint::aliases::U256;
 use std::collections::HashMap;
-use autonomi::{Client, PublicKey};
-use serde::{Serialize, Deserialize};
+use autonomi::{SecretKey, Client, PublicKey, client::payment::PaymentOption, XorName, Chunk, Bytes, GraphEntry};
+use serde::{Serialize, Deserialize, Serializer, Deserializer,
+	de::{Visitor, MapAccess, value::MapAccessDeserializer}
+};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct TokenInfo {
@@ -10,20 +13,156 @@ pub struct TokenInfo {
 	decimals: u8,
 }
 
-pub type Wallet = HashMap<DerivationIndex, Vec<(PublicKey, U256)>>;
-// TODO: give index key a name?
-// TODO: optional pubkey ("none" meaning waiting for payment)?
+#[derive(Clone, Debug)]
+pub struct Wallet(SecretKey, HashMap<DerivationIndex, Vec<(PublicKey, U256)>>);
+// TODO: ? give index key a name
+// TODO: ? optional pubkey ("none" meaning waiting for payment)? this would require supplying amount arg for request.
+// TODO: read/write wallet to autonomi: serde.
+// TODO: read/write wallet to bytes: serde.
+
+impl Wallet {
+	pub fn new(sk: SecretKey) -> Self { // TODO: maybe PublicKey will be sufficient?
+		Self(sk, HashMap::new())
+	}
+	
+	pub fn request(&mut self, index: &DerivationIndex) -> PublicKey {
+		let receiver_sk = self.0.derive_child(&index.0);
+		let receiver_key = receiver_sk.public_key();
+		println!("request BLS SecretKey: {:.4}(...)", receiver_sk.to_hex());
+		println!("request Derived PublicKey: {:.4}(...), {:?}", receiver_key.to_hex(), receiver_key);
+
+		if self.1.get(index).is_none() {
+			self.1.insert(index.clone(), vec![]);
+		}
+
+		receiver_key
+	}
+
+	pub fn receive(&mut self, amount: U256, spend: PublicKey, index: &DerivationIndex) {
+		match self.1.get_mut(index) {
+			Some(transactions) => {
+				transactions.push((spend, amount));
+			},
+			None => {
+				self.1.insert(index.clone(), vec![(spend, amount)]);
+			}
+		};
+		// TODO: validate
+	}
+
+//	pub fn find(&self, request: PublicKey) -> DerivationIndex {
+//		// TODO: find index by deriving and comparing with all available indexes
+//	}
+}
+
+impl Serialize for Wallet {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: Serializer,
+	{
+		// do not serialize SecretKey
+		self.1.serialize(serializer)
+	}
+}
+
+impl<'de> Deserialize<'de> for Wallet {
+	fn deserialize<D>(deserializer: D) -> Result<Wallet, D::Error>
+	where
+		D: Deserializer<'de>,
+	{
+		struct WalletVisitor;
+
+		impl <'de> Visitor<'de> for WalletVisitor {
+			type Value = Wallet;
+
+			fn expecting(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+				write!(f, "a map")
+			}
+
+			fn visit_map<A>(self, map: A) -> Result<Wallet, A::Error>
+			where A: MapAccess<'de> {
+				MapAccessDeserializer::new(map).deserialize_map(self)
+			}
+		}
+
+		deserializer.deserialize_map(WalletVisitor)
+	}
+}
+
+
+pub trait WalletExt {
+//	fn act_wallet_get(sk: SecretKey) -> Result<Wallet, String>;
+}
+
+impl WalletExt for Client {
+//	fn act_wallet_get(sk: SecretKey) -> Result<Wallet, String> {
+//		// TODO
+//	}
+}
+
 
 #[derive(Clone, Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
-struct DerivationIndex(Vec<u8>);
+pub struct DerivationIndex(Vec<u8>);
 
 
-trait ActExt {
-	// TODO: token operations
+pub trait ActExt {
+	fn act_create(
+		&self,
+		name: String,
+		symbol: String,
+		decimals: u8,
+		total_supply: U256,
+		issuer_key: PublicKey,
+		payment: &PaymentOption,
+	) -> impl std::future::Future<Output = Result<(PublicKey, XorName), String>> + Send;
 }
 
 impl ActExt for Client {
-	// TODO: token operations implementation
+
+	async fn act_create(
+		&self,
+		name: String,
+		symbol: String,
+		decimals: u8,
+		total_supply: U256,
+		issuer_key: PublicKey,
+		payment: &PaymentOption,
+	) -> Result<(PublicKey, XorName), String> {
+
+		// create token info chunk
+		let token_info = Chunk::new(Bytes::from(serde_json::to_string(&TokenInfo {
+			name,
+			symbol,
+			decimals,
+		}).map_err(|e| format!("{}", e))?));
+		let (_paid, token_info_address) = self.chunk_put(&token_info, payment.clone())
+			.await.map_err(|e| format!("{}", e))?;
+
+		println!("TokenInfo Chunk: {}", token_info_address);
+
+		let token_id = token_info_address.xorname();
+		println!("TokenId: {}", token_id);
+
+		// create genesis tx with output to given key
+
+		let genesis_owner = SecretKey::random();
+		let genesis_owner_pubkey = genesis_owner.public_key();
+		let genesis = GraphEntry::new(
+			&genesis_owner,
+			vec![],
+			token_id.0.clone(),
+			vec![(issuer_key, total_supply.to_be_bytes())] // all output to issuer
+		);
+		let (_paid, genesis_address) = self.graph_entry_put(genesis, payment.clone())
+			.await.map_err(|e| format!("{}", e))?;
+
+		println!("Genesis GraphEntry: {}", genesis_address);
+		let genesis_spend = *genesis_address.owner();
+
+		assert_eq!(genesis_owner_pubkey, genesis_spend);
+
+		Ok((genesis_spend, *token_id))
+	}
 }
 
 
@@ -31,9 +170,7 @@ impl ActExt for Client {
 #[cfg(test)]
 mod tests {
 	use sn_curv::elliptic::curves::ECScalar;
-	use autonomi::{SecretKey, Wallet as EvmWallet, Chunk, Bytes, GraphEntry, GraphEntryAddress,
-		client::payment::PaymentOption
-	};
+	use autonomi::{Wallet as EvmWallet, GraphEntryAddress};
 	use tracing::Level;
 	use super::*;
 
@@ -101,60 +238,33 @@ mod tests {
 			hex::decode(EVM_PK).map_err(|e| format!("{}", e))?[0..32].try_into().unwrap()
 		).expect("Wrong bytes").serialize().into()).expect("Wrong bytes");
 
-		let index = DerivationIndex(vec![1]);
-		let issuer_sk = sk.derive_child(&index.0);
-		let issuer_key = issuer_sk.public_key();
-		println!("Issuer BLS SecretKey: {:.4}(...)", issuer_sk.to_hex());
-		println!("Issuer Derived PublicKey: {:.4}(...), {:?}", issuer_key.to_hex(), issuer_key);
 
-		// create token info chunk
-
-		let token_info = Chunk::new(Bytes::from(serde_json::to_string(&TokenInfo {
-			symbol: "AANT".into(),
-			name: "Alternative Autonomi Network Token".into(),
-			decimals: DECIMALS,
-		}).map_err(|e| format!("{}", e))?));
-		let (_paid, token_info_address) = client.chunk_put(&token_info, with_wallet.clone())
-			.await.map_err(|e| format!("{}", e))?;
-
-		println!("TokenInfo Chunk: {}", token_info_address);
-
-		// create genesis tx with output to given key
+		let mut wallet = Wallet::new(sk.clone());
+		let issuer_index = DerivationIndex(vec![1]);
+		let issuer_key = wallet.request(&issuer_index);
+		let issuer_sk = sk.derive_child(&issuer_index.0);
+		println!("Wallet: {:?}", wallet);
 
 		let total_supply: U256 = amount(1_000_000, DECIMALS);
-		let genesis_owner = SecretKey::random();
-		let genesis_owner_pubkey = genesis_owner.public_key();
-		let genesis = GraphEntry::new(
-			&genesis_owner,
-			vec![],
-			token_info_address.xorname().clone().0,
-			vec![(issuer_key, total_supply.to_be_bytes())] // all output to issuer
-		);
-		let (_paid, genesis_address) = client.graph_entry_put(genesis, with_wallet.clone())
-			.await.map_err(|e| format!("{}", e))?;
-
-		println!("Genesis GraphEntry: {}", genesis_address);
-		assert_eq!(&genesis_owner_pubkey, genesis_address.owner());
+		let (genesis_spend, token_id) = client.act_create(
+			"Alternative Autonomi Network Token".into(),
+			"AANT".into(),
+			DECIMALS,
+			total_supply,
+			issuer_key,
+			&with_wallet,
+		).await?;
 
 		// populate wallet struct
 
-		let mut wallet = Wallet::new();
-		match wallet.get_mut(&index) {
-			Some(transactions) => {
-				transactions.push((*genesis_address.owner(), total_supply));
-			},
-			None => {
-				wallet.insert(index, vec![(*genesis_address.owner(), total_supply)]);
-			}
-		};
-
+		wallet.receive(total_supply, genesis_spend, &issuer_index);
 		println!("Wallet: {:?}", wallet);
 
 		// TODO: save wallet to scratchpad
 
 		// check balance on that key
 
-		let tx = client.graph_entry_get(&GraphEntryAddress::new(*genesis_address.owner()))
+		let tx = client.graph_entry_get(&GraphEntryAddress::new(genesis_spend))
 			.await.map_err(|e| format!("{}", e))?;
 		let (balance, overflow) = tx.descendants.iter()
 			.filter(|(pubkey, _data)| pubkey == &issuer_key)
@@ -173,34 +283,18 @@ mod tests {
 		let receive_amount = amount(200, DECIMALS);
 
 		let receive_index = DerivationIndex(vec![2]);
-		let receiver_sk = sk.derive_child(&receive_index.0);
-		let receiver_key = receiver_sk.public_key();
-		println!("Receiver BLS SecretKey: {:.4}(...)", receiver_sk.to_hex());
-		println!("Receiver Derived PublicKey: {:.4}(...), {:?}", receiver_key.to_hex(), receiver_key);
-
-		if wallet.get(&receive_index).is_none() {
-			wallet.insert(receive_index.clone(), vec![]);
-		}
-
+		let receive_key = wallet.request(&receive_index);
 		println!("Wallet: {:?}", wallet);
 
 		// prepare rest output
 
 		let rest_index = DerivationIndex(vec![3]);
-		let rest_sk = sk.derive_child(&rest_index.0);
-		let rest_key = rest_sk.public_key();
-		println!("Rest BLS SecretKey: {:.4}(...)", rest_sk.to_hex());
-		println!("Rest Derived PublicKey: {:.4}(...), {:?}", rest_key.to_hex(), rest_key);
-
-		if wallet.get(&rest_index).is_none() {
-			wallet.insert(rest_index.clone(), vec![]);
-		}
-
+		let rest_key = wallet.request(&rest_index);
 		println!("Wallet: {:?}", wallet);
 
 		// spend
 
-		let inputs_amounts = wallet.remove(&DerivationIndex(vec![1])).expect("There should be inputs");
+		let inputs_amounts = wallet.1.remove(&DerivationIndex(vec![1])).expect("There should be inputs");
 		let (mut inputs, sum, overflow) = inputs_amounts.iter()
 			.fold(
 				(Vec::<PublicKey>::new(), U256::from(0), false),
@@ -212,16 +306,16 @@ mod tests {
 				}
 			);
 		assert!(!overflow); // TODO: error/result
-		inputs.insert(0, *genesis_address.owner());
+		inputs.insert(0, genesis_spend);
 		println!("Inputs: {:?}", (&inputs, sum, overflow));
 
 		let rest_amount = sum.checked_sub(receive_amount).unwrap();
 		let spend = GraphEntry::new(
 			&issuer_sk,
 			inputs,
-			token_info_address.xorname().clone().0,
+			token_id.0,
 			vec![
-				(receiver_key, receive_amount.to_be_bytes()),
+				(receive_key, receive_amount.to_be_bytes()),
 				(rest_key, rest_amount.to_be_bytes())
 			] // all output to issuer
 		);
@@ -231,30 +325,14 @@ mod tests {
 		println!("Spend GraphEntry: {}", spend_address);
 		assert_eq!(&issuer_key, spend_address.owner());
 
-		match wallet.get_mut(&rest_index) {
-			Some(transactions) => {
-				transactions.push((*spend_address.owner(), rest_amount));
-			},
-			None => {
-				wallet.insert(rest_index.clone(), vec![(*spend_address.owner(), rest_amount)]);
-			}
-		};
-
-		// receive
-
-		match wallet.get_mut(&receive_index) {
-			Some(transactions) => {
-				transactions.push((*spend_address.owner(), receive_amount));
-			},
-			None => {
-				wallet.insert(receive_index.clone(), vec![(*spend_address.owner(), rest_amount)]);
-			}
-		};
-
+		wallet.receive(rest_amount, *spend_address.owner(), &rest_index);
+		wallet.receive(receive_amount, *spend_address.owner(), &receive_index);
 		println!("Wallet: {:?}", wallet);
 
 		Ok(())
 	}
+
+	// TODO: de/serialze wallet
 
 	#[test]
 	fn pk_cannot_be_arbitrary_32bytes() -> Result<(), String> {
@@ -274,10 +352,20 @@ mod tests {
 	}
 
 	#[test]
-	fn can_reference_erc20() -> Result<(), String> {
-		// TODO
-		assert_eq!(0, 0);
+	fn pubkey_from_derived_sk_equals_derived_pubkey() {
+		let index = "index1_abcdef";
+		println!("index: {:?}", index);
+		let sk = SecretKey::random();
+		println!("sk: {:?}", sk);
+		let pk = sk.public_key();
+		println!("pk: {:?}", pk);
 
-		Ok(())
+		let pubkey_from_derived_sk = sk.derive_child(index.as_bytes()).public_key();
+		println!("pubkey_from_derived_sk: {:?}", pubkey_from_derived_sk);
+		let derived_pubkey = pk.derive_child(index.as_bytes());
+		println!("derived_pubkey: {:?}", derived_pubkey);
+
+		assert_eq!(pubkey_from_derived_sk, derived_pubkey);
 	}
+
 }
